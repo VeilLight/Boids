142c37321b0fef9c911c23f30cce293478d2f332
António Ferreira <asim.ferreira@gmail.com>
2023-05-22 20:33:47 +0100

    Add cohesion principle
    - Limit boid speed between a maximum and a minimum

--- boids2d/boid.gd
+++ boids2d/boid.gd
@@ -1,34 +1,31 @@
 extends Polygon2D
 
+# The distance at which boids are able to see each other.
+const NEIGHBOURHOOD_DISTANCE = 100
+
 var screen_width = 0
 var screen_height = 0
 var velocity = Vector2()
+var max_speed = 0
+var min_speed = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var size = get_window().size
 	screen_width = size.x
 	screen_height = size.y
+	max_speed = get_meta("max_speed")
+	min_speed = get_meta("min_speed")
+	
 	# Choose a random direction to start flying into
 	var angle = randf_range(0, 2 * PI)
 	update_velocity(Vector2(cos(angle), sin(angle)))
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	translate(velocity * get_meta("Speed"))
+func _process(_delta):
+	translate(velocity)
 	if _out_of_screen_bounds():
 		_navigate_to_other_side_of_screen()
-
-func _navigate_to_other_side_of_screen():
-	if position.x > screen_width:
-		set_position(Vector2(0, position.y))
-	elif position.x < 0:
-		set_position(Vector2(screen_width, position.y))
-
-	if position.y > screen_height:
-		set_position(Vector2(position.x, 0))
-	elif position.y < 0:
-		set_position(Vector2(position.x, screen_height))
 
 func _out_of_screen_bounds():
 	return position.x > screen_width \
@@ -36,7 +33,24 @@ func _out_of_screen_bounds():
 		|| position.y > screen_height \
 		|| position.y < 0
 
-func update_velocity(velocity: Vector2):
-	self.velocity = velocity
+func _navigate_to_other_side_of_screen():
+	if position.x > screen_width:
+		set_position(Vector2(0, position.y))
+	elif position.x < 0:
+		set_position(Vector2(screen_width, position.y))
+	if position.y > screen_height:
+		set_position(Vector2(position.x, 0))
+	elif position.y < 0:
+		set_position(Vector2(position.x, screen_height))
+
+func update_velocity(_velocity: Vector2):
+	velocity += _velocity
+	
+	# Limit speed between a minimum and maximum
+	var velocity_magnitude = velocity.length()
+	var speed = max(min(velocity_magnitude, get_meta("max_speed")), get_meta("min_speed"))
+	velocity = velocity / velocity_magnitude * speed
+	
 	# Update model rotation to match the new flight direction
-	rotate(velocity.angle() - deg_to_rad(90))
+	set_rotation(velocity.angle() + deg_to_rad(90))
+
--- boids2d/boids_2d.gd
+++ boids2d/boids_2d.gd
@@ -1,12 +1,18 @@
 extends Node2D
 
+# The size of the flock, a.k.a the total number of boids
+const FLOCK_SIZE = 100
+# The amount of approximation towards the neighbours center of mass.
+# The lower the number the higher the approximation gets.
+const COHESION_FACTOR = 500
+
 var boid_scene = preload("res://boids2d/boid.tscn")
 var flock = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var size = get_window().size
-	for i in range(150):
+	for i in range(FLOCK_SIZE):
 		var boid = boid_scene.instantiate()
 		var initial_x = randf_range(0, size.x)
 		var initial_y = randf_range(0, size.y)
@@ -15,5 +21,34 @@ func _ready():
 		add_child(boid)
 		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _process(_delta):
+	for boid in flock:
+		var separation = _apply_separation(boid)
+		var alignment = _apply_alignment(boid)
+		var cohesion = _apply_cohesion(boid)
+		var velocity = separation + alignment + cohesion
+		boid.update_velocity(velocity)
+
+func _apply_separation(boid):
+	return Vector2()
+
+func _apply_alignment(boid):
+	return Vector2()
+
+func _apply_cohesion(boid):
+	var avg_neighbours_positions = Vector2()
+	var neighbours = 0
+	
+	for b in flock:
+		if b == boid:
+			continue
+		var distance = boid.position.distance_to(b.position)
+		if distance < boid.NEIGHBOURHOOD_DISTANCE:
+			avg_neighbours_positions += b.position
+			neighbours += 1
+	
+	if neighbours > 0:
+		var center_of_mass_position = avg_neighbours_positions / neighbours
+		return (center_of_mass_position - boid.position) / COHESION_FACTOR
+	
+	return Vector2()

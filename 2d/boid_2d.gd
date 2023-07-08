e0325f559cd88f2cc59bd772cc21ffd7d955f0f5
António Ferreira <VeilLight@users.noreply.github.com>
2023-07-08 20:27:36 +0100

    Tweak flocking parameters calculations - factor times weight
    - Add weights to each flocking parameter calculation to manage how much should each parameter affects the flocking behaviour.
    - Add slider to control boid speed
    - Take into account frame time when moving boids
    - Increase window size

--- 2d/boid_2d.gd
+++ 2d/boid_2d.gd
@@ -1,26 +1,37 @@
 extends Polygon2D
 
-var max_width = 0
-var max_height = 0
-var velocity = Vector2()
+const IN_BOUNDS_TURN_FACTOR: float = 0.8
+
+var window_width: int = 0
+var window_height: int = 0
+var window_margin: int = 0
+var speed: int = 0
+var velocity: Vector2 = Vector2()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	max_width = get_window().size.x
 	# Choose a random direction to start flying into
 	var angle = randf_range(0, 2 * PI)
 	update_velocity(Vector2(cos(angle), sin(angle)))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	translate(velocity)
-	_limit_velocity()
+func _process(_delta: float):
+	velocity *= speed * _delta
+	_limit_speed()
 	_update_rotation()
 	_update_bounds()
+	translate(velocity)
 
-func setup(_initial_position: Vector2, _max_height: int):
+func set_inital_position(_initial_position: Vector2):
 	set_position(_initial_position)
-	max_height = _max_height
+
+func set_speed(_speed: int):
+	speed = _speed
+
+func set_window_bounds(_window_width:int, _window_height: int, _window_margin: int):
+	window_height = _window_height
+	window_width = _window_width
+	window_margin = _window_margin
 
 func update_velocity(_velocity: Vector2):
 	velocity += _velocity
@@ -32,38 +43,35 @@ func _update_rotation():
 	# Update model rotation to match the flight direction
 	set_rotation(velocity.angle() + deg_to_rad(90))
 
-func _limit_velocity():
+func _limit_speed():
 	# Limit speed between a minimum and maximum
 	var velocity_magnitude = velocity.length()
-	var speed = max(min(velocity_magnitude, get_meta("max_speed")), get_meta("min_speed"))
-	velocity = velocity / velocity_magnitude * speed
+	var clamped_speed = max(min(velocity_magnitude, speed), speed - 1)
+	velocity = velocity / velocity_magnitude * clamped_speed
 	
 func _update_bounds():
-	var screen_margin = get_meta("screen_margin")
-	if screen_margin > 0:
-		_stay_within_scree_bounds(screen_margin)
+	if window_margin > 0:
+		_stay_within_screen_bounds()
 	else:
 		_navigate_to_other_side_of_screen_if_out_of_bounds()
 
-func _stay_within_scree_bounds(screen_margin: float):
-	var turn_factor = get_meta("turn_factor")
+func _stay_within_screen_bounds():
+	if position.x < window_margin:
+		velocity.x += IN_BOUNDS_TURN_FACTOR
+	elif position.x > window_width - window_margin:
+		velocity.x -= IN_BOUNDS_TURN_FACTOR
 	
-	if position.x < screen_margin:
-		velocity.x += turn_factor
-	elif position.x > max_width - screen_margin:
-		velocity.x -= turn_factor
-	
-	if position.y < screen_margin:
-		velocity.y += turn_factor
-	elif position.y > max_height - screen_margin:
-		velocity.y -= turn_factor
+	if position.y < window_margin:
+		velocity.y += IN_BOUNDS_TURN_FACTOR
+	elif position.y > window_height - window_margin:
+		velocity.y -= IN_BOUNDS_TURN_FACTOR
 
 func _navigate_to_other_side_of_screen_if_out_of_bounds():
-	if position.x > max_width:
+	if position.x > window_width:
 		set_position(Vector2(0, position.y))
 	elif position.x < 0:
-		set_position(Vector2(max_width, position.y))
-	if position.y > max_height:
+		set_position(Vector2(window_width, position.y))
+	if position.y > window_height:
 		set_position(Vector2(position.x, 0))
 	elif position.y < 0:
-		set_position(Vector2(position.x, max_height))
+		set_position(Vector2(position.x, window_height))
--- 2d/flock_2d.gd
+++ 2d/flock_2d.gd
@@ -1,9 +1,12 @@
 extends Node2D
 
 # The size of the flock, i.e. the total number of boids
-const FLOCK_SIZE = 100
+const FLOCK_SIZE: int = 100
 # The distance from the window bounds at which boids will spawn
-const BOID_SPAWN_MARGIN = 50
+const BOID_SPAWN_MARGIN: int = 55
+const SEPARATION_WEIGHT: float = 0.05
+const ALIGNMENT_WEIGHT: float = 0.35
+const COHESION_WEIGHT: float = 0.06
 
 var flock = []
 
@@ -18,20 +21,26 @@ func _ready():
 		var boid = boid_scene.instantiate()
 		var initial_x = randf_range(BOID_SPAWN_MARGIN, max_x)
 		var initial_y = randf_range(BOID_SPAWN_MARGIN, max_y)
-		boid.setup(Vector2(initial_x, initial_y), max_y)
+		boid.set_inital_position(Vector2(initial_x, initial_y))
+		boid.set_window_bounds(max_x, max_y, BOID_SPAWN_MARGIN)
+		boid.set_speed($Properties/BoidSpeed/HSlider.value)
 		flock.append(boid)
 		add_child(boid)
-		
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	for boid in flock:
-		var separation = _apply_separation(boid)
-		var alignment = _apply_alignment(boid)
-		var cohesion = _apply_cohesion(boid)
+		var separation: Vector2 = _apply_separation(boid)
+		var alignment: Vector2 = _apply_alignment(boid)
+		var cohesion: Vector2 = _apply_cohesion(boid)
 		var velocity: Vector2 = separation + alignment + cohesion
 		
 		if !velocity.is_equal_approx(Vector2.ZERO):
 			boid.update_velocity(velocity)
+
+func _on_boid_speed_value_changed(speed: int):
+	for boid in flock:
+		boid.set_speed(speed)
 
 func _apply_separation(boid):
 	var separation = Vector2()
@@ -46,7 +55,12 @@ func _apply_separation(boid):
 		
 		separation += to_other
 	
-	return separation / $Properties/Separation/HSlider.value
+	var separation_factor = _get_slider_percentage($Properties/Separation/HSlider)
+	if separation_factor == 0:
+		return Vector2()
+	
+	separation_factor *= SEPARATION_WEIGHT
+	return separation * separation_factor
 
 func _apply_alignment(boid):
 	var avg_neighbours_velocities = Vector2()
@@ -67,7 +81,12 @@ func _apply_alignment(boid):
 		return Vector2()
 	
 	var avg_velocity = avg_neighbours_velocities / neighbours
-	return (avg_velocity - boid.get_velocity()) / $Properties/Alignment/HSlider.value
+	var alignment_factor = _get_slider_percentage($Properties/Alignment/HSlider)
+	if alignment_factor == 0:
+		return Vector2()
+	
+	alignment_factor *= ALIGNMENT_WEIGHT
+	return (avg_velocity - boid.get_velocity()) * alignment_factor
 
 func _apply_cohesion(boid):
 	var avg_neighbours_positions = Vector2()
@@ -88,4 +107,12 @@ func _apply_cohesion(boid):
 		return Vector2()
 	
 	var center_of_mass_position = avg_neighbours_positions / neighbours
-	return (center_of_mass_position - boid.position) / $Properties/Cohesion/HSlider.value
+	var cohesion_factor = _get_slider_percentage($Properties/Cohesion/HSlider)
+	if cohesion_factor == 0:
+		return Vector2()
+		
+	cohesion_factor *= COHESION_WEIGHT
+	return (center_of_mass_position - boid.position) * cohesion_factor
+
+func _get_slider_percentage(slider: HSlider):
+	return slider.value / slider.max_value
